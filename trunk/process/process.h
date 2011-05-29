b0dd5d1b82687cd854a4baae316f31e9105dbc02
usmankhan9190@gmail.com <usmankhan9190@gmail.com@0d70b4c8-45ed-2677-aac3-7b04d5109aca>
2011-05-29 04:22:23 +0000

    Moved itoa function to its on file

--- trunk/process/process.h
+++ trunk/process/process.h
@@ -10,24 +10,40 @@
 
 #include "../shared/rtx_inc.h"
 #include "../dbug/dbug.h"
+#include "../util/util.h"
 
-#define NULL_PROCESS_ID			0;
+#define NULL_PROCESS_ID			0
 
 /* Process Priorities */
-#define PRIORITY_0				0;
-#define PRIORITY_1				1;
-#define PRIORITY_2				2;
-#define PRIORITY_3				3;
-#define NULL_PROCESS_PRIORITY	4;
+#define PRIORITY_0				0
+#define PRIORITY_1				1
+#define PRIORITY_2				2
+#define PRIORITY_3				3
+#define NULL_PROCESS_PRIORITY	4
 
 /* Process states */
-#define BLOCKED_STATE			0;
-#define READY_STATE				1;
+#define STATE_NEW				0
+#define STATE_READY				1
+#define STATE_BLOCKED			2
+
+/* Blocked types */
+#define BLOCK_NONE				0
+#define BLOCK_MEMORY			1
+#define BLOCK_RECEIVE			2
+
+/* Create ready and blocked queues of given size */
+int create_queues(int size);
+
+/* Add process to ready queue */
+int add_to_ready(int process_ID);
+
+/* Add process to blocked queue */
+int add_to_blocked(int processs_ID);
 
 /* Set process priority to given value */
-int set_process_priority( int process_ID, int priority ); 
+int set_process_priority(int process_ID, int priority); 
 
 /* Return priority for given process */
-int get_process_priority( int process_ID );
+int get_process_priority(int process_ID);
 
 #endif /* _PROCESS_H_ */
---
+++ trunk/process/run.py
@@ -0,0 +1,60 @@
+#/usr/bin/env python
+import pexpect, sys, os, time, dircache
+
+TIMEOUT=10
+serverfilename = 'u3khan.s19'
+
+# compile our code
+if os.system('make') != 0:
+		print 'Error compiling code'
+else:
+		print 'Compilation successful!'
+
+# find .s19 file
+list = dircache.listdir(".")
+s19file = None
+for file in list:
+	extension = os.path.splitext(file)[1]
+	if extension == '.s19':
+		s19file = file
+
+# exit if .s19 file was not found
+if s19file is None:
+	print 'Unable to find s19 file. Exiting...'
+	sys.exit(0)
+
+# copy our stuff to cfserver
+if os.system('cfcp %s %s > /dev/null' % (s19file, serverfilename)) != 0:
+        print 'Error copying firmware'
+else:   
+        print 'Successfully copied %s to cf-server as %s' %(s19file, serverfilename)
+
+cfserver = pexpect.spawn('telnet cf-server')
+
+# grab the 2nd serial port info
+cfserver.expect('Second Serial Port On Port: ([0-9]*\n)')
+PORT = int(cfserver.match.group(1))
+
+# select "continue executing" in cf-server
+cfserver.expect('Please Pick', timeout=TIMEOUT)
+cfserver.expect(': ', timeout=TIMEOUT)
+cfserver.send('1')
+print "Started JanusROM"
+time.sleep(1)
+
+# wait for the janusROM prompt to init tftp
+cfserver.expect('janusROM> ', timeout=TIMEOUT)
+cfserver.send('tftp -f %s\r\n' % serverfilename)
+cfserver.expect('Finished. Start Address is [0-9a-fA-Fx]+\.', timeout=TIMEOUT)
+print "Successfully loaded %s" % serverfilename
+
+# make your life easier
+print "Second serial port on port: %i" % PORT
+print "telnet cf-server %i" % PORT
+cfserver.interact()
+
+# clean up
+if os.system('make clean') != 0:
+		print 'Error cleaning up'
+else:
+		print 'Clean up successful!'
