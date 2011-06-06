326b43ea4064b7d6bf39e77321c51dafa3ea90c4
ank3it@gmail.com <ank3it@gmail.com@0d70b4c8-45ed-2677-aac3-7b04d5109aca>
2011-06-06 19:40:54 +0000

    added trap handler & modified rtx.c:(not de-bugged) v1

---
+++ trap/trap.h
@@ -0,0 +1,20 @@
+/**
+ * @group: S11-G22
+ * @file: trap.h
+ * @date: 2011/06/06
+ * @brief: Header file for trap.c
+ */
+
+#ifndef _TRAP_H_
+#define _TRAP_H_
+
+#include "../dbug/dbug.h"
+
+void c_trap_handler( void );
+void setpr(int value);
+int __main( void );
+int main( void );
+
+
+
+#endif /* _TRAP_H_ */
