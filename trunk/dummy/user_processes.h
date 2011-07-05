4a5432aed9d4fb0d933888d5ee3d847bd0e01b55
s2menon <s2menon@0d70b4c8-45ed-2677-aac3-7b04d5109aca>
2011-07-05 18:12:41 +0000

    added user processes

---
+++ trunk/dummy/user_processes.h
@@ -0,0 +1,21 @@
+#ifndef _USER_PROCESSES_H_
+#define _USER_PROCESSES_H_
+
+#include "envelope.h"
+
+extern void ProcessA();
+extern void ProcessB();
+extern void ProcessC();
+extern void enqueue_local_message(envelope *message);
+
+
+typedef struct temp_msg_queue
+{
+	struct envelope *node;
+	struct tmp_msg_queue *next;
+};
+
+struct temp_msg_queue *temp_message_head;
+struct temp_msg_queue *temp_message_tail;
+
+#endif
